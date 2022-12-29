a6bb32d65d43da88063c71f4658ab37227d22bd7
TRK360 <117139528+TRK360@users.noreply.github.com>
2022-12-29 14:31:13 -0500

    Assignment_5  Using While and Do_While

---
+++ Assignment_5/Assignment_5/Assignment_5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Assignment_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char x = 'a';// declaring an initialization of variable x
+
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            } while (x <= 'f');//sets boundry for writeline function to stop executing for do-while statement
+
+
+            Console.WriteLine("Please enter a value between 1 and 10");//seeks user input
+            int n = Convert.ToInt32(Console.ReadLine());
+            while (++n < 11)//sets boundry for while statement
+            {
+                Console.WriteLine("Newest value of n is {0}", n);//monitorable activity
+            }
+        }
+    }
+}
