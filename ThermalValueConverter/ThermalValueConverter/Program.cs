56f0a7134d1ff17b68c8965300c2d721746efade
TRK360 <117139528+TRK360@users.noreply.github.com>
2023-01-02 14:14:34 -0500

    Assignment_10 Class and Methods

---
+++ ThermalValueConverter/ThermalValueConverter/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ThermalValueConverter
+{
+    class TestTemperatureConverter
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("---Welcome to a thermal analysis program---");
+        Console.WriteLine("Enter a value in Celsius to convert to Fahrenheit and more...");
+        //Console.WriteLine("2. From Fahrenheit to Celsius.");
+       
+
+        string? selection = "1";
+        double F, C, L= 0;
+
+            Console.WriteLine("Please enter a numerical value of which you want to convert");
+
+            string userTemp = Console.ReadLine();
+
+
+
+        switch (selection)
+        {
+            case "1":
+                F = TemperatureConverter.CelsiusToFahrenheit(userTemp ?? "0");
+                Console.WriteLine("Temperature in Fahrenheit: {0:F2}", F);
+
+                C = TemperatureConverter.FahrenheitToCelsius(userTemp ?? "0");
+                Console.WriteLine("Temperature in Celsius (IF FAHRENHEIGHT ENTERED ERRONIOUSLY): {0:F2}", C);
+
+                    L = TemperatureConverter.MultipliedTemps(userTemp ?? "0");
+                    Console.WriteLine("Starting Temp times 100 equals:   {0:F2}", L);
+
+                    break;
+
+            default:
+                Console.WriteLine("Please select a convertor.");
+                break;
+        }
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ThermalValueConverter/ThermalValueConverter/TemperatureConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThermalValueConverter
+{
+    public static class TemperatureConverter
+    {
+        public static double CelsiusToFahrenheit(string temperatureCelsius)
+        {
+            // Convert argument to double for calculations.
+            double celsius = Double.Parse(temperatureCelsius);
+
+            // Convert Celsius to Fahrenheit.
+            double fahrenheit = (celsius * 9 / 5) + 32;
+
+            return fahrenheit;
+        }
+
+        public static double FahrenheitToCelsius(string temperatureFahrenheit)
+        {
+            // Convert argument to double for calculations.
+            double fahrenheit = Double.Parse(temperatureFahrenheit);
+
+            // Convert Fahrenheit to Celsius.
+            double celsius = (fahrenheit - 32) * 5 / 9;
+
+            return celsius;
+        }
+
+        public static double MultipliedTemps(string fahrenheight)
+        {
+            // Convert argument to double for calculations.
+            double fahrenheit100 = Double.Parse(Convert.ToString(fahrenheight+"00"));
+
+            // Convert Fahrenheit to Celsius.
+            double cnew = (fahrenheit100);
+
+            return cnew;
+        }
+
+
+    }
+}
