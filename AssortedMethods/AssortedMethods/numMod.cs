97753c7d2d44acb58c5baa7681f70212d2a04059
TRK360 <117139528+TRK360@users.noreply.github.com>
2023-01-03 18:59:20 -0500

    Assignment_11 sumbittal

---
+++ AssortedMethods/AssortedMethods/numMod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssortedMethods
+{
+    class numMod
+    {
+        // adding two integer values.
+        public int Add(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+
+        // adding three integer values.
+        public Decimal Multiply(Decimal a)
+        {
+            int sum = Convert.ToInt32((a * a * a));
+            return sum;
+        }
+
+        // adding four integer values.
+        public string Power4(string a)
+        {
+            int b = Convert.ToInt32(a);
+         
+            string reSum = Convert.ToString(b*b*b*b);
+            return reSum;
+        }
+
+    }
+}
