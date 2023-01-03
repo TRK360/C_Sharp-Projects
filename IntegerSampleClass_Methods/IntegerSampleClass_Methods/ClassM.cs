cdb2f41dac2170f207a2ae34d542e3dc4a015703
TRK360 <117139528+TRK360@users.noreply.github.com>
2023-01-03 11:47:20 -0500

    Cust Class with several Methods

---
+++ IntegerSampleClass_Methods/IntegerSampleClass_Methods/ClassM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntegerSampleClass_Methods
+{
+    public class ClassM
+    {
+
+        // adding two integer values.
+        public int Add(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+
+        // adding three integer values.
+        public int Multiply(int a, int b, int c)
+        {
+            int sum = a * b * c;
+            return sum;
+        }
+
+        // adding four integer values.
+        public int Power4(int a, int b, int c, int d)
+        {
+            int sum = a^4;
+            return sum;
+        }
+
+
+    }
+}
---
+++ IntegerSampleClass_Methods/IntegerSampleClass_Methods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace IntegerSampleClass_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a single digit value:\n");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            ClassM ob = new ClassM();
+
+            int sum1 = ob.Add(n, n);
+            Console.WriteLine("your entry plus itself: "
+                              + "\nResult: " + sum1);
+
+            int sum2 = ob.Multiply(n, n, n);
+            Console.WriteLine("your entry times itself 3 times: "
+                              + "\nResult: " + sum2);
+            int sum3 = ob.Power4(n, n, n, n);
+            Console.WriteLine("your entry to the power of four: "
+                              + "\nResult:: " + sum3);
+        }
+    }
+}
