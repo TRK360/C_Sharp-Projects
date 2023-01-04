b7174859516a711c9ebd6404627833dbfea167e4
TRK360 <117139528+TRK360@users.noreply.github.com>
2023-01-04 10:31:10 -0500

    Assignment_11 Update

---
+++ AssortedMethods/AssortedMethods/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AssortedMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+                Console.WriteLine("Please enter a single digit value:\n");
+                int n = Convert.ToInt32(Console.ReadLine());//user input converted as req'd below
+                Decimal dId = Convert.ToDecimal(n);
+                string sId = Convert.ToString(n);
+
+                numMod ob = new numMod();//referencing new instance for numMod class
+
+
+
+                int sum1 = ob.mathOp(n, n);//applying parameters which can be used by class
+                Console.WriteLine("\nyour entry plus itself: "
+                                  + "\nResult: " + sum1);
+
+
+
+
+                int sum2 = Convert.ToInt32(ob.mathOp(dId));//applying parameters which can be used by class
+            Console.WriteLine("\nyour entry times itself 3 times: "
+                                  + "\nResult: " + sum2);
+
+
+
+                int sum3 = ob.mathOp(sId);//applying parameters which can be used by class
+                int sum4 = Convert.ToInt32(sum3);// converting to applicable input format for class )(dictated by desired method)
+
+                Console.WriteLine("\nyour entry to the power of four: "
+                                  + "\nResult: " + (sum4));
+
+            Console.ReadLine();
+
+            }
+        }
+    }
+
---
+++ AssortedMethods/AssortedMethods/numMod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssortedMethods
+{
+    class numMod
+    {
+        // applyingtwo integer values.
+        public int mathOp(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+
+        // applying three decimal values.
+        public int mathOp(Decimal a)
+        {
+            int sum = Convert.ToInt32((a * a * a));
+            return sum;
+        }
+
+        // adding string and converting to integer to return as a string result.
+        public int mathOp(string a)
+        {
+            int b = Convert.ToInt32(a);
+         
+            int reSum = b*b*b*b;
+
+            return reSum;
+        }
+
+    }
+}
