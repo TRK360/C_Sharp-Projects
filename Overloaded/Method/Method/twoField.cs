e8a56270b75e2ffc2e2cc0690e45820639ba07ec
TRK360 <117139528+TRK360@users.noreply.github.com>
2023-01-03 20:01:59 -0500

    Method with Multiple needed inputs

---
+++ Overloaded/Method/Method/twoField.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method
+{
+    public class twoField
+    {
+
+        // adding two integer values.
+        public int Add(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+
+
+
+    }
+}
