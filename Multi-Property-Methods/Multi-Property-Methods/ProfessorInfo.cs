42367f73ddef90338e7be65997172e10d90bfccf
TRK360 <117139528+TRK360@users.noreply.github.com>
2023-01-05 16:22:38 -0500

    Assignment Submittal

---
+++ Multi-Property-Methods/Multi-Property-Methods/ProfessorInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multi_Property_Methods
+{
+    public static class ProfessorInfo//everything below is a static entry to the professor type paired with receiving input parameters by name structure
+    {
+        // static field  
+        public static string Subject = "History";
+        // private fields  
+        private static string name;
+        private static string school;
+        private static int rank;
+        private static int years;
+        // static properties  
+        public static int Years { get => years; set => years = value; }
+        public static int Rank { get => rank; set => rank = value; }
+        public static string School { get => school; set => school = value; }
+        public static string Name { get => name; set => name = value; }
+        // static method  
+        public static int CalculateScore(int rank, int years)
+        {
+            return rank * years;
+        }
+    }
+}
