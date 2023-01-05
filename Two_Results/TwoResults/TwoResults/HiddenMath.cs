a9c9d0a83b24411129be87bdf12193eb340b8b1a
TRK360 <117139528+TRK360@users.noreply.github.com>
2023-01-05 10:38:12 -0500

    Methods including Named Parameters

---
+++ Two_Results/TwoResults/TwoResults/HiddenMath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoResults
+{
+    public class HiddenMath
+    {
+
+
+        public int HidOb2(int numa, int numb)
+        {
+            int hidden = numa * 12;//not used for end-user but could be incorporated behind the scenes if ever needed
+
+            int visible = numb;//hidden; (to test that main program receives differenet inputs properly since not being entered manually)
+            return visible;
+        }
+
+
+        public int HidOb(int num1, int num2)
+        {
+            
+            int hidden = num1*num2;// simple multiplication of two pre-set int values
+
+
+            int trial = Convert.ToInt32(hidden);
+
+            return trial;
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Two_Results/TwoResults/TwoResults/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace TwoResults
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter one SINGLE DIGIT number followed by a second...(PRESS ENTER TO SUBMIT EACH NUMBER (4 KEYSTROKES TOTAL)");
+            int uEntry1 = Convert.ToInt32(Console.ReadLine());//user inputs first value to apply
+            int uEntry2 = Convert.ToInt32(Console.ReadLine());//user inputs second value to apply
+
+            Console.WriteLine("Input accepted!\n\n---PROCEEDING WITH PROGRAM---");
+
+            HiddenMath ob = new HiddenMath();// calls up class and inherent method
+
+            int sum1 = ob.HidOb2(uEntry1, uEntry2);//called up to produce result
+
+            Console.WriteLine("Although there are some additional math results available, we are only passing along the following: " + sum1 + "\n\n---PROCEEDING TO FINAL PART OF PROGRAM---");
+
+            Console.ReadLine();
+
+
+            Console.WriteLine("Please observe below to witness a pre-set named parameter value times another one:");/// beginning portion where pre-named parameters are manually assigned values
+
+            int y = 0;//setting integers to later be modified
+            int z = 0;
+
+            Console.WriteLine("PRE-SELECTED inputs accepted!\n\n---PROCEEDING WITH PROGRAM---");
+
+            //HiddenMath ob = new HiddenMath();// testing point: calling up class and inherent method
+
+            ob.HidOb(y=13, z=10);// assigning values to parameters x and y
+            int sum2 = ob.HidOb(y,z);//applying method with inputs specified above
+
+            Console.WriteLine("The PRE-SELECTED inputs multiplied by one another is:    "+sum2+ "\n\n---ENDING PROGRAM NOW---");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
