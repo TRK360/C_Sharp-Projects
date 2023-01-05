11a5ea004cc39f4bdf7096062b552de21144fcc9
TRK360 <117139528+TRK360@users.noreply.github.com>
2023-01-05 15:07:34 -0500

    Updates Multiple Methods and Parameter types

---
+++ Multi-Property-Methods/Multi-Property-Methods/getValue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multi_Property_Methods
+{
+
+    class getValue
+    {
+
+        public void NumberManipulator(out int x)//declares the below as output
+        {
+            int temp = 5;
+            x = temp;
+
+
+
+
+
+        }
+    }
+
+}
