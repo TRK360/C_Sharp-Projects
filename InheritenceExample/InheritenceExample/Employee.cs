90a00068145ee10170273fbb36be8b1ded5f6a19
TRK360 <117139528+TRK360@users.noreply.github.com>
2023-01-05 19:40:56 -0500

    Assignment_13

---
+++ InheritenceExample/InheritenceExample/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritenceExample
+{
+    public class Employee:Person
+    {
+
+        public List<int> EmpID { get; set; }
+        public int eNumber { get; set; }
+
+
+        public void Testingalot()
+
+        {
+
+            foreach (int fom in EmpID)
+               
+                {
+                    
+
+                    Console.WriteLine(fom);
+
+                }
+
+
+    }
+
+
+    }
+}
