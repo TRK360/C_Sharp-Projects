7a3cd4a4ea23188bab3abe59d6631fbe4a5fd63f
TRK360 <117139528+TRK360@users.noreply.github.com>
2023-01-06 10:41:56 -0500

    Assignment_14 Abstract Inheritence

---
+++ AbstractInheritence/AbstractInheritence/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractInheritence
+{
+    public class Employee : Person
+    {
+
+
+        public int eNumber { get; set; }///receivable input at time of logging a specific type of person (person is abstract)
+
+        public override void Append() { throw new NotImplementedException(); }
+
+
+        public override void SayName() {
+            Console.WriteLine("Adding in an element specific to this person type:  \n\nEmployee\n\n\n---ID#:  -"+eNumber+"-  ---\n\n");
+            base.SayName();/// calling in the base elements SayName,  override here acknowledges contract between Employee class and parent class
+                
+                }
+
+
+
+
+
+
+
+    }
+}
---
+++ AbstractInheritence/AbstractInheritence/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractInheritence
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Employee personb = new Employee();//calling up non-abstract instance
+            personb.eNumber = 176;
+            personb.FirstName = "roy";
+            personb.LastName = "smith";///all are enterable for use by assorted methods
+            personb.SayName();// result of interactions from both class and base class
+            Console.ReadLine();
+
+           
+        }
+    }
+    }
+
