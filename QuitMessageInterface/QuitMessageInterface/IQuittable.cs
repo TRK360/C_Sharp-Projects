2d78f724875a909599c9e04840cda1f0a429b05d
TRK360 <117139528+TRK360@users.noreply.github.com>
2023-01-06 12:14:38 -0500

    Assignment 15

---
+++ QuitMessageInterface/QuitMessageInterface/IQuittable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuitMessageInterface
+{
+    interface IQuittable//denotes format which we receive info
+    {
+        // method without body
+        void Quit(int l);
+
+
+
+    }
+}
---
+++ QuitMessageInterface/QuitMessageInterface/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace QuitMessageInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+                Employee personb = new Employee();//calling up non-abstract instance
+                personb.eNumber = 176;
+                personb.FirstName = "roy";
+                personb.LastName = "smith";///all are enterable for use by assorted methods
+                personb.Quit(100); 
+                personb.SayName();// result of interactions from both class and base class
+                Console.ReadLine();
+
+            //Shape r1 = new Shape();
+
+            //r1.calculateArea(100, 200);
+
+        }
+    }
+}
