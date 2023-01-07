474e5734de1b7723fc1cb1ff2b707ac0e069814d
TRK360 <117139528+TRK360@users.noreply.github.com>
2023-01-07 10:18:22 -0500

    Assignment_16

---
+++ DoubleEqualsOverload/DoubleEqualsOverload/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoubleEqualsOverload
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+
+        //public bool Equals(Employee other)
+        //{
+        //    throw new NotImplementedException();
+
+        //}
+
+
+        public static bool operator ==(Employee emp1, Employee emp2)///uses == operator to compare integer inputs and return a boolean
+        {
+            if (emp1.Id.Equals(emp2.Id))
+            {
+                return true;/// boolean reaction
+
+            }
+            return false;  /// alt boolean reaction if unequal in value
+        }
+
+        public static bool operator !=(Employee emp1, Employee emp2)///needed offset or pair to above == analysis
+        {
+            if (!emp1.Id.Equals(emp2.Id))
+            {
+                return true;
+
+            }
+            return false;
+
+
+        }
+
+        
+
+
+    }
+}
---
+++ DoubleEqualsOverload/DoubleEqualsOverload/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DoubleEqualsOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Employee emp1 = new Employee();///enter employee1 details here
+            emp1.Id = 1;
+            emp1.FirstName = "Bob";// helpful for different a alysis, not for this demo
+            emp1.LastName = "Smith";// helpful for different a alysis, not for this demo
+
+            Employee emp2 = new Employee();///enter employee1 details here
+            emp2.Id = 1;/// intentionally same as above (if changed, booean results will change)
+            emp2.FirstName = "Bob";
+            emp2.LastName = "Smith";
+
+
+            if (emp1 == emp2)// properties/method for employee comparison
+            {
+
+                Console.WriteLine("these cats and kittens are IDENTICAL");
+
+            }
+
+            else
+            {
+                Console.WriteLine("these individuals are all UNIQUE");
+            }
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
