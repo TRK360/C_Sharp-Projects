6d084b8fed7042f40290502badfc5facf1b86855
TRK360 <117139528+TRK360@users.noreply.github.com>
2023-01-07 14:23:14 -0500

    Days of Week In loose class with Enum

---
+++ ColorsInquiry/ColorsInquiry/Weekdays.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ColorsInquiry
+{
+    public class Weekdays
+    {
+
+        enum DayofWeek { Sunday, Monday, tueSDAY, Wednesday, thuRSDAY, Friday, Saturday }
+
+    }
+}
