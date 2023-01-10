827a757582b72c40e43c5feed77e2d34f9ed7dca
TRK360 <117139528+TRK360@users.noreply.github.com>
2023-01-10 10:51:57 -0500

    Assignment_19

---
+++ ConstructorChain/ConstructorChain/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConstructorChain
+{
+    
+       class mClass
+        {
+            public mClass()/// each declared class ahead of actual program is developed with constructors with chained classes per below
+            {
+                Console.WriteLine("ONE");
+            }
+
+            public mClass(int x) : this()
+            {
+                Console.WriteLine("TWO");
+            }
+
+            public mClass(int x, int y) : this(100)
+            {
+                Console.WriteLine("TWO AND A HALF....");
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                mClass c = new mClass(100, 20);//instantiates a new object which begins leaning on the constructors in the classes above
+                Console.Read();
+            
+       
+            const string units = "SF";// string constant Units is a good candidate for a const
+            var area = "150"; //implicit reference to string
+
+            Console.WriteLine("The square footage value of a space 15 ft x 10 ft will always be:  " + area + "  " + units);
+
+
+        }
+    }
+}
