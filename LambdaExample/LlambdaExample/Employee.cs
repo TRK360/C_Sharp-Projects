313e693123a16c32c48c4c26ba0a9d2c8c426bc3
TRK360 <117139528+TRK360@users.noreply.github.com>
2023-01-10 09:19:01 -0500

    Assignment_17 Lambda Functions

---
+++ LambdaExample/LlambdaExample/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LlambdaExample
+{
+    public class Employee
+    {
+
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public int eNumber { set; get; }
+        
+}
+    }
